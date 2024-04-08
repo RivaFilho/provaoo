629ae06a32fe929e522d962723a6e94b15e9f9b9
RivaFilho <128065143+RivaFilho@users.noreply.github.com>
2024-04-08 13:52:18 -0300

    Update ex1.java

--- ex1.java
+++ ex1.java
@@ -25,4 +25,13 @@ import javax.swing.JOptionPane;public class Questao3a { public static void main(
     System.out.println(r);
 }
 } vai entrar  5, 2 e3
-*/
+*/ public class Questao3a { public static void main(String[] args) { int[] x = new int[]{1, 0, 1, 1, 0, 0, 1}; int y = 4; int z = 0; for (int i = x.length; i > 0; i--) { z += calcular(y, Math.abs(i - x.length), x[i - 1]); } System.out.println(z); }static int calcular(int k, int l, int m) {
+    int j = 1;
+    for (int i = l; i > 0; i--) {
+        j *= k;
+    }
+    j *= m;
+    System.out.println(j);
+    return j;
+}
+}
